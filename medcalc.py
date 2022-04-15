3662f77eae65d3260ed178b5624b5ef378742ed4
shingochan <42294998+shingochan@users.noreply.github.com>
2022-04-16 01:20:49 +0900

    日本時間に変更
    ブラウザで見た時に標準時刻になるバグを修正

--- medcalc.py
+++ medcalc.py
@@ -1,15 +1,17 @@
 import streamlit as st
 import datetime
-from datetime import date
-from datetime import timedelta
+from datetime import date, timedelta, timezone
+
+jst = timezone(timedelta(hours=9), 'JST')
+dt_now = datetime.datetime.now(jst)
+jst_today = datetime.date(dt_now.year, dt_now.month ,dt_now.day)
+
 st.markdown("##### リフィル計算機")
-
-rxdate = st.date_input("調剤日を入力してください",value=datetime.date.today())
+rxdate = st.date_input("調剤日を入力してください",value=jst_today)
 diff_day = st.number_input("処方日数を入力してください",min_value=1,value=28)
+
 rxday = timedelta(days= diff_day)
-
 rxday2 = rxdate + rxday
-
 rxday_min = rxday2 - timedelta(days = 7)
 rxday_max = rxday2 + timedelta(days = 7)
 
