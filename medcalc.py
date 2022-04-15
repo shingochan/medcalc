1ba93c015546f1c23342f5bc4685ea8cf819ae31
shingochan <42294998+shingochan@users.noreply.github.com>
2022-04-16 00:17:13 +0900

    軽微な変更
    テキスト部分の軽微な変更を行いました。

--- medcalc.py
+++ medcalc.py
@@ -2,9 +2,7 @@ import streamlit as st
 import datetime
 from datetime import date
 from datetime import timedelta
-
-st.title("リフィル計算機")
-
+st.markdown("##### リフィル計算機")
 
 rxdate = st.date_input("調剤日を入力してください",value=datetime.date.today())
 diff_day = st.number_input("処方日数を入力してください",min_value=1,value=28)
@@ -15,6 +13,6 @@ rxday2 = rxdate + rxday
 rxday_min = rxday2 - timedelta(days = 7)
 rxday_max = rxday2 + timedelta(days = 7)
 
-st.write(
-    f"次回調剤予定日は{rxday2}です。リフィル可能な期間は{rxday_min}から{rxday_max}までです。"
+st.markdown(
+    f"次回調剤予定日：**{rxday2}**  \nリフィル対応期間：**{rxday_min}**〜**{rxday_max}**  \n"
     )
