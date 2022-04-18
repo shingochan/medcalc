e6be4a2139a0ada870acedfe7d2c3e73f6e1cad8
shingochan <42294998+shingochan@users.noreply.github.com>
2022-04-18 23:12:19 +0900

    クレアチニンクリアランス計算機を追加しました
    クレアチニンクリアランス計算機を追加しました

--- medcalc.py
+++ medcalc.py
@@ -16,5 +16,23 @@ rxday_min = rxday2 - timedelta(days = 7)
 rxday_max = rxday2 + timedelta(days = 7)
 
 st.markdown(
-    f"次回調剤予定日　：**{rxday2}**  \nリフィル対応期間：**{rxday_min}**〜**{rxday_max}**  \n"
+    f"　次回調剤予定日　：**{rxday2}**  \nリフィル対応期間：**{rxday_min}**〜**{rxday_max}**  \n"
     )
+
+st.markdown("# ")
+# クレアチニンクリアランス
+st.markdown("##### クレアチニンクリアランス計算機")
+display = ("男性", "女性")
+options = list(range(len(display)))
+gender = st.selectbox("性別を選んでください。", options, format_func=lambda x: display[x])
+age = st.number_input('年齢を入力してください。',value=75,min_value=0,max_value=120)
+weight = st.number_input('体重を入力してください。',value=60,min_value=0,max_value=200)
+if gender == 0:
+    gender_coefficient=1
+else:
+    gender_coefficient=0.85
+
+Cre = st.number_input('クレアチニン値を入力してください。',value=0.80,min_value=0.00,max_value=10.00,step=0.01)
+
+CCr=round((140-age)*weight*gender_coefficient/(72*Cre),2)
+st.markdown(f'クレアチニンクリアランス：{CCr}')
