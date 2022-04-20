f3b0a338ea462a875466fc4f798e8fa26359fc99
shingochan <42294998+shingochan@users.noreply.github.com>
2022-04-21 07:47:18 +0900

    Update medcalc.py

--- medcalc.py
+++ medcalc.py
@@ -31,9 +31,9 @@ if print_layout:
     st.markdown(
     f"　次回来局予定日は  \n**{rxday2.month}月{rxday2.day}日**です。  \nリフィル調剤可能な期間は  \n**{rxday_min.month}月{rxday_min.day}日**から**{rxday_max.month}月{rxday_max.day}日**です。  \n"
     )
-    st.sidebar.write('サイドバーを閉じてからラベルプリンタで印刷してください。')
+    st.sidebar.write('サイドバーを閉じて、ラベルプリンタで印刷してください。')
 else:
-    st.write('リフィル計算機です。サイドバーに調剤日と処方日数を入力してください。')
+    st.markdown('リフィル計算機です。  \n**>**を押してサイドバーに調剤日と処方日数を入力してください。')
     st.markdown(
         f"　次回調剤予定日：**{rxday2}**  \nリフィル対応期間：**{rxday_min}**〜**{rxday_max}**  \n"
         )
