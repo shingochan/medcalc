074148a5db17f26513e8bbfd6b465fa52d519f02
shingochan <42294998+shingochan@users.noreply.github.com>
2022-04-21 02:12:51 +0900

    Update medcalc.py

--- medcalc.py
+++ medcalc.py
@@ -7,6 +7,7 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
+            section.main div.block-container {padding: 1rem 1rem 1rem !important;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
